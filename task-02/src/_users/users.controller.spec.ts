469807e50fd53bfbb6a09b09654c15c4e0018a80
Karol Wilczak <karol.wilczak@vp.pl>
2024-04-10 05:47:38 +0200

    Added unit test for the account creation endpoint in the usersService and usersController.

--- task-02/src/_users/users.controller.spec.ts
+++ task-02/src/_users/users.controller.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+  let cacheManager: { get: jest.Mock; set: jest.Mock };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: 'CACHE_MANAGER',
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
+    cacheManager = module.get('CACHE_MANAGER') as any;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('SignUp', () => {
+    it('Should sign Up an user', async () => {
+      const signUpDto = {
+        email: 'user@gmail.com',
+        pass: 'password123',
+        firstName: 'User 1',
+        lastName: 'Wilkowski',
+        phoneNumber: 123456789,
+        shirtSize: 34,
+        preferredTechnology: 'Node',
+      };
+
+      const mockData = {
+        id: '1',
+        firstName: 'User 1',
+        email: 'user@gmail.com',
+        hashedPass: '12sbdt38dy3',
+        createdAt:
+          'Wed Apr 10 2024 00:20:33 GMT+0200 (Central European Summer Time)',
+        lastName: 'Wilkowski',
+        phoneNumber: 123456789,
+        shirtSize: 34,
+        preferredTechnology: 'Node',
+        token: '',
+      };
+      jest.spyOn(service, 'signUp').mockResolvedValueOnce(mockData);
+
+      const result = await controller.signUp(signUpDto);
+      expect(result).toEqual(mockData);
+    });
   });
 });
--- task-02/src/_users/users.service.spec.ts
+++ task-02/src/_users/users.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import axios from 'axios';
+import { ForbiddenException } from '@nestjs/common';
+
+jest.mock('axios');
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    jest.resetAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
+  describe('signUp', () => {
+    const signUpDto = {
+      email: 'user@gmail.com',
+      pass: 'password123',
+      firstName: 'User 1',
+      lastName: 'Wilkowski',
+      phoneNumber: 123456789,
+      shirtSize: 34,
+      preferredTechnology: 'Node',
+    };
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    const mockData = {
+      id: '1',
+      firstName: 'User 1',
+      email: 'user@gmail.com',
+      hashedPass: '12sbdt38dy3',
+      createdAt:
+        'Wed Apr 10 2024 00:20:33 GMT+0200 (Central European Summer Time)',
+      lastName: 'Wilkowski',
+      phoneNumber: 123456789,
+      shirtSize: 34,
+      preferredTechnology: 'Node',
+      token: '',
+    };
+    it('should sign Up an user and create user object', async () => {
+      (axios.post as jest.Mock).mockResolvedValueOnce({ data: mockData });
+
+      const result = await service.signUp(signUpDto);
+      expect(result).toEqual(mockData);
+    });
+
+    it('should throw Forbidden Exception if user with given email already exist', async () => {
+      (axios.post as jest.Mock).mockResolvedValueOnce({
+        response: { status: 403 },
+      });
+
+      await expect(service.signUp(signUpDto)).rejects.toThrowError(
+        ForbiddenException,
+      );
+    });
   });
 });
